c3fd22b6d30a4081fe9e1fd4c3215a68cc0d11ec
Bhumi Prajapati <bhumikprajapati007@gmail.com>
2026-08-19 19:36:13 +0530

    feat: initialize project with Alembic migrations

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL:str
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+settings = Settings()
+
+DATABASE_URL = settings.DATABASE_URL
