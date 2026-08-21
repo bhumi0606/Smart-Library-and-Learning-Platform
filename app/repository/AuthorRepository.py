1c79afc2ba413f626495546ad39491501ba740dd
Bhumi Prajapati <bhumikprajapati007@gmail.com>
2026-08-21 12:48:20 +0530

    feat: implement repositories

---
+++ app/repository/AuthorRepository.py
@@ -0,0 +1,70 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.models.Author import Author
+from app.schemas.author.AuthorCreate import AuthorCreate
+from app.schemas.author.AuthorUpdate import AuthorUpdate
+
+async def create_author(
+        author: AuthorCreate,
+        session: AsyncSession
+):
+    author = Author(
+        name = author.name
+    )
+
+    await session.add(author)
+    await session.commit()
+    return author
+
+async def get_author_by_id(
+        id: int,
+        session: AsyncSession
+):
+    author = await session.execute(
+        select(Author).where(Author.id == id)
+    )
+
+    return author.scalar_one_or_none()
+
+async def get_authors(
+        session: AsyncSession
+):
+    authors = await session.execute(
+        select(Author)
+    )
+    return authors.scalars().all()
+
+async def update_author(
+        id: int,
+        update_author: AuthorUpdate,
+        session: AsyncSession
+):
+    author = await get_author_by_id(
+        id = id,
+        session = session
+    )
+    if author == None:
+        return None
+    else:
+        if update_author.name:
+            author.name = update_author.name
+
+    await session.commit()
+    return author
+
+async def delete_author(
+        id:int,
+        session: AsyncSession
+):
+    author = await get_author_by_id(
+        id = id,
+        session = session
+    )
+    if author == None:
+        return None
+
+    await session.delete(author)
+    await session.commit()
+
+    return author
+    
---
+++ app/repository/BookRepository.py
@@ -0,0 +1,83 @@
+from app.db.models.Book import Book
+from app.schemas.book.BookCreate import CreateBook
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.schemas.book.BookUpdate import UpdateBook
+
+
+async def create_book(
+    book: CreateBook,
+    session: AsyncSession
+):
+    book = Book(
+        title = book.title,
+        published_date = book.published_date,
+        isbn = book.isbn,
+        book_type = book.book_type,
+        status = book.status
+    )
+
+    await session.add(book)
+    await session.commit()
+    return book
+
+async def get_books(
+        session: AsyncSession
+):
+    books = await session.execute(
+        select(Book)
+    )
+    return books.scalars().all()
+
+async def get_book_by_id(
+        id: int,
+        session: AsyncSession
+):
+    book = await session.execute(
+        select(Book).where(Book.id == id)
+    )
+    return book.scalar_one_or_none()
+
+async def update_book(
+        id: int,
+        update_book: UpdateBook,
+        session: AsyncSession
+):
+    book = await get_book_by_id(
+        id = id,
+        session = session
+    )
+
+    if book == None:
+        return None
+    else:
+        if update_book.title:
+            book.title = update_book.title
+        if update_book.published_date:
+            book.published_date = update_book.published_date
+        if update_book.isbn:
+            book.isbn = update_book.isbn
+        if update_book.book_type:
+            book.book_type = update_book.book_type
+        if update_book.status:
+            book.status = update_book.status
+
+    await session.commit()
+    return book
+
+async def delete_book(
+        id: int,
+        session: AsyncSession
+):
+    book = await get_book_by_id(
+        id = id,
+        session = session
+    )
+    if book == None:
+        return None
+
+    await session.delete(book)
+    await session.commit()
+
+    return book
